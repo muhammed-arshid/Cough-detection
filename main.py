f5a410c5b1d7dcb13041fd1f5c18e26ec0d492a0
Muhammed Arshid <50525900+muhammed-arshid@users.noreply.github.com>
2020-05-22 13:03:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import joblib,os
+import sounddevice
+import scipy
+from scipy.io.wavfile import write
+def voice_recording():
+    fs = 44100
+    sec = 10
+    print("recording...")
+    recod_voic = sounddevice.rec(int(sec *fs),samplerate=fs,channels=2)
+    sounddevice.wait()
+    a = scipy.io.wavfile.write("out.wav",fs,rec_voic)
+    print("finished.........")
+def main():
+    st.title('Alpha Ai Solution')
+    st.subheader('Cough Detection Web Application')
+    activites = ["Members","mem 1","mem 1","mem 1","mem 1"]
+    choice = st.sidebar.selectbox("Alpha Team",activites)
+
+    status = st.radio("Activate the App",("Start","Stop"))
+    if status == "Start" :
+        st.success("its Activated")
+        voice_recording()
+    if status == "Stop":
+        st.error("Stoped the fuction ")
