d2fc14da6c46015582b16df70157da8033ded6ee
Muhammed Arshid <50525900+muhammed-arshid@users.noreply.github.com>
2020-06-10 00:41:47 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,54 +3,37 @@ import joblib,os
 import numpy as np
 import librosa
 import noisereduce as nr
-from tensorflow.keras.models import model_from_json
+from tensorflow.keras.models import load_model
+from tensorflow.keras import backend as K
 import pyaudio
 import webbrowser
 import wave 
 
-
-#error
-@st.cache(allow_output_mutation=True)
-def load_model():
-    model_name = "/home/arshid/Desktop/pro/updated_model"
-    
-    # Model reconstruction from JSON file
-    with open( model_name + '.json', 'r') as f:
-        model = model_from_json(f.read())
-    # Load weights into the new model
-    model.load_weights( model_name + '.h5')
-    return model
-
-#error
-
-
-
 def predictSound(X):
     y, sr = librosa.load(X)
-    noisy_part = y[10000:]
-    reduced_noise = nr.reduce_noise(audio_clip=y, noise_clip=noisy_part, verbose=False)  # this line is not using in model so we are getting issue
+    noisy_part = y[8000:]
+    y = nr.reduce_noise(audio_clip=y, noise_clip=noisy_part, verbose=False)
     spect = librosa.feature.melspectrogram(y=y, sr=sr,n_fft=2048, hop_length=512)
     spect = librosa.power_to_db(spect, ref=np.max)
     spect = np.expand_dims(spect, axis = -1)
     spect = np.expand_dims(spect, axis = 0)
-
-    #err0r
-    model1 = load_model()
-    res = model1.predict(np.array(spect))
-    res = np.round(res)
-    #error
-
-    return res
-def main():
-    st.title('Alpha Ai Solution')
-    st.subheader('Cough Detection Web Application')
+    return np.array(spect)
+def model1():
+    model_weights_path = "./weights1.h5"
+    model_path = "./model.h5"
+    model = load_model(model_path)
+    model.load_weights(model_weights_path)
+    return model
+if __name__ == "__main__":
+    st.title("Alpha Ai Solution")
+    st.subheader("Cough Detection Web Application")
     activites = ["Members","mem 1","mem 1","mem 1","mem 1"]
-    choice = st.sidebar.selectbox("Alpha Team",activites)             # i  think this is also not works
+    choice = st.sidebar.selectbox("Alpha Team",activites)
     status = st.radio("Activate the App",("Start","Stop"))
     if status == "Start" :
         st.success("its Activated")
-              
-        while(True):
+        t = True
+        while(t):
             FORMAT = pyaudio.paFloat32
             CHANNELS = 2
             RATE = 22050
@@ -69,17 +52,17 @@ def main():
             waveFile.setframerate(RATE)
             waveFile.writeframes(b''.join(frames))
             waveFile.close()
-            file1 = '/home/arshid/Desktop/pro/file.wav'   #where to store for other pc
+            file1 = './file.wav'
             res  = predictSound(file1)
-            print(res)
-            if res[0][0] == 1 :#expected output to give condition
+            model =model1()
+            predictions = np.round(model.predict(res))
+            print(predictions)
+            if predictions[0][0] == 1 :#expected output to give condition
                 webbrowser.open('http://coughdetect.c1.biz/', new=2)
-        
+                t = False
             stream.stop_stream()
             stream.close()
             p.terminate()
         # voice_recording()
     if status == "Stop":
         st.error("Stoped")
-if __name__ == "__main__":
-    main()
